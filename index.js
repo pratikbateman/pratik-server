927da77e8758ba46b73fb951c68f0c3183835227
pratikbateman <143090228+pratikbateman@users.noreply.github.com>
2024-12-02 01:19:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 import express, { urlencoded, json } from 'express';
 const app = express();
 
-app.use(urlencoded({ extended: true }))
-app.use(json())
+app.use(urlencoded({ extended: true }));
+app.use(json());
 
 app.get('/', (req, res) => {
-    res.send('Hello Pratik Ghosh!');
+    res.send('Hello, Pratik Ghosh!');
 });
 
 app.post('/data', (req, res) => {
     const data = req.body;
-    res.send(`You sent: ${JSON.stringify(data)}`);
+    console.log(`Received string: ${data.message}`); // Print received message
+    res.send(`You sent: ${data.message}`);
 });
 
-
 app.post('/endpoint', (req, res) => {
-    console.log('Received data:', req.body);
+    console.log('Received data:', req.body.message);
+    console.log('Received successfully');
     res.status(200).send('Data received successfully');
 });
 
